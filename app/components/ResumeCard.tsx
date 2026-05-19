e430ed024f3036c48db33a6878f8f8fad6ae91b9
Ferdinand <fmmphahle01@gmail.com>
2026-05-19 17:23:35 +0200

    Fetch real resumes on homepage

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router";
 import ScoreCircle from "./ScoreCircle";
+import { usePuterStore } from "~/lib/puter";
 
 const ResumeCard = ({
   resume: { id, companyName, jobTitle, feedback, imagePath },
 }: {
   resume: Resume;
 }) => {
+  const { auth, fs } = usePuterStore();
+  const [resumeUrl, setResumeUrl] = useState("");
+
+  useEffect(() => {
+    const loadResumes = async () => {
+      const blob = await fs.read(imagePath);
+      if (!blob) return;
+      let url = URL.createObjectURL(blob);
+      setResumeUrl(url);
+    };
+
+    loadResumes();
+  }, [imagePath]);
+
   return (
     <Link
       to={`/resume/${id}`}
@@ -14,27 +29,36 @@ const ResumeCard = ({
     >
       <div className="resume-card-header">
         <div className="flex flex-col gap-1.5 min-w-0">
-          <h2 className="text-black! font-bold break-words leading-tight">
-            {companyName}
-          </h2>
-          <h3 className="text-base break-words text-gray-500 leading-snug">
-            {jobTitle}
-          </h3>
+          {companyName && (
+            <h2 className="text-black! font-bold break-words leading-tight">
+              {companyName}
+            </h2>
+          )}
+          {
+            <h3 className="text-base break-words text-gray-500 leading-snug">
+              {jobTitle}
+            </h3>
+          }
+          {!companyName && !jobTitle && (
+            <h2 className="text-black! font-bold">Resume</h2>
+          )}
         </div>
         <div className="shrink-0 ml-2">
           <ScoreCircle score={feedback.overallScore} />
         </div>
       </div>
 
-      <div className="gradient-border flex-1">
-        <div className="w-full h-full">
-          <img
-            src={imagePath}
-            alt={`${companyName} resume`}
-            className="w-full h-[300px] object-cover object-top rounded-xl"
-          />
+      {resumeUrl && (
+        <div className="gradient-border flex-1">
+          <div className="w-full h-full">
+            <img
+              src={resumeUrl}
+              alt={`${companyName} resume`}
+              className="w-full h-[300px] object-cover object-top rounded-xl"
+            />
+          </div>
         </div>
-      </div>
+      )}
     </Link>
   );
 };
