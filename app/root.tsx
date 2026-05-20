7e71902ae02d2936710989f61281cd7414fd4c98
Ferdinand <fmmphahle01@gmail.com>
2026-05-20 13:47:58 +0200

    analytics

--- app/root.tsx
+++ app/root.tsx
@@ -10,6 +10,7 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 import { usePuterStore } from "./lib/puter";
+import { Analytics } from "@vercel/analytics/next";
 import { useEffect } from "react";
 
 export const links: Route.LinksFunction = () => [
@@ -42,6 +43,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
         {children}
         <ScrollRestoration />
+        <Analytics />
         <Scripts />
       </body>
     </html>
