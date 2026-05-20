72a648ed5f0175eeeab91b1aa1a766e37ab347f8
Ferdinand <fmmphahle01@gmail.com>
2026-05-20 13:42:21 +0200

    upd

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ScoreBadge from "./ScoreBadge";
-import ScoreGauge from "./scoreGauge";
+import ScoreGauge from "./ScoreGauge";
 
 const getScoreColor = (score: number) => {
   if (score > 70) return "#059669";
